2f8f9469a5a09da637dbfbc0494b3eae86efb5eb
Jayson Gazeley <89006649+jgazeley@users.noreply.github.com>
2021-09-02 12:14:39 -0700

    Update wifilock.py

--- wifilock.py
+++ wifilock.py
@@ -1,67 +1,29 @@
 from subprocess import *
-import RPi.GPIO as io
+from honda import unlock, lock
 import time
 
-io.setmode(io.BCM)
-io.setwarnings(False)
+def get_wifi():
+  shell_cmd = 'iwconfig {} | grep Link'.format('wlan0')
+  proc = Popen(shell_cmd, shell=True, stdout=PIPE, stderr=PIPE)
+  output, err = proc.communicate()
+  msg = output.decode('utf-8').strip()
+  try:
+    wifi = int(msg[13] + msg[14])
+    return wifi
+  except:
+    return False
 
-UNLK = 16
-LOCK = 12
-PLIGHTS = 6
-#HORN = 21
-
-io.setup(UNLK, io.OUT)
-io.setup(LOCK, io.OUT)
-io.setup(PLIGHTS, io.OUT)
-#io.setup(HORN, io.OUT)
-io.output(UNLK, 1)
-io.output(LOCK, 1)
-io.output(PLIGHTS, 1)
-#io.output(HORN, 1)
-
-def getWifi():
-    shell_cmd = 'iwconfig {} | grep Link'.format('wlan0')
-    proc = Popen(shell_cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    output, err = proc.communicate()
-    msg = output.decode('utf-8').strip()
-    try:
-        wifi = int(msg[13] + msg[14])
-        return wifi
-    except:
-        return False
-
-def getLocked():
-    with open("/home/pi/lockstat.txt", "r") as LOCKED:
-        if '1' in LOCKED.read():
-            return True
+def get_locked():
+  with open("/home/pi/lockstat", "r") as lockstat:
+    if '1' in lockstat.read():
+      return True
 
 while True:
 
-    if getLocked():
-        if getWifi() >= 10:
-            io.output(UNLK, 0)
-            io.output(PLIGHTS, 0)
-            time.sleep(.1)
-            io.output(UNLK, 1)
-            time.sleep(.6)
-            io.output(PLIGHTS, 1)
-            time.sleep(.7)
-            io.output(PLIGHTS, 0)
-            time.sleep(.7)
-            io.output(PLIGHTS, 1)
-            with open("/home/pi/lockstat.txt", "w") as LOCKED:
-                LOCKED.write('LOCKED = 0')
-    else:
-        if getWifi() < 10:
-            io.output(LOCK, 0)
-            io.output(PLIGHTS, 0)
-            time.sleep(.1)
-            io.output(LOCK, 1)
-#           io.output(HORN, 0)
-            time.sleep(.1)
-#           io.output(HORN, 1)
-            time.sleep(.5)
-            io.output(PLIGHTS, 1)
-            with open("/home/pi/lockstat.txt", "w") as LOCKED:
-                LOCKED.write('LOCKED = 1')
-    time.sleep(1)
+  if get_locked():
+    if get_wifi() >= 10:
+      unlock()
+  else:
+    if get_wifi() < 10:
+      lock()
+  time.sleep(1)
